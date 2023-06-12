0437e3d89af9e96865735d06aa027fb1abc2d62d
DJ322 <72959116+DJ322@users.noreply.github.com>
2023-06-12 14:36:25 -0500

    Terraform LAMP stack
    ### !!! Read Me First !!! ###
    # This project is for display purposes only #
    # These files are designed only as a representation of the architecture used #
    # Please contact me for working code #
    ### !!! Thank You !!! ###

--- Project-2-Coffee/aurora_mysql.tf
+++ Project-2-Coffee/aurora_mysql.tf
@@ -3,14 +3,14 @@
 # This file is designed only as a representation of the architecture used #
 ### !!! Thank You !!! ###
 
-# Create an RDS database cluster (Aurora Serverless)
+# RDS/Aurora-MySQL database cluster (Aurora Serverless)
 resource "aws_rds_cluster" "rds_cluster" {
   cluster_identifier     = "coffee-web-db-cluster"
   engine                 = "aurora-mysql"
   engine_mode            = "provisioned"
   engine_version         = "8.0"
-  master_                = "$"
-  master_                = "$" 
+  master_username        = "admin"
+  master_password        = "12345678"   # !! Password Must be set to random before use !! #
   database_name          = "wordpress"
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   db_subnet_group_name   = aws_db_subnet_group.aws_db_subnet_group.name
@@ -21,7 +21,7 @@ resource "aws_rds_cluster" "rds_cluster" {
   }
 }
 
-# Create an RDS database instance
+#  RDS database instance
 resource "aws_rds_cluster_instance" "rds_cluster" {
   cluster_identifier = aws_rds_cluster.rds_cluster.cluster_identifier
   instance_class     = "db.serverless"
@@ -30,33 +30,43 @@ resource "aws_rds_cluster_instance" "rds_cluster" {
 
 }
 
-# Create an RDS database subnet
+# Private RDS database subnet for east-1a  & east-1b
 resource "aws_db_subnet_group" "aws_db_subnet_group" {
   name       = "coffe_aws_db_subnet_group"
-  subnet_ids = [aws_subnet.subnet5.id, aws_subnet.subnet6.id]
+  subnet_ids = [aws_subnet.pvt_sbnet_rds_1a.id, aws_subnet.pvt_sbnet_rds_1b.id]
 }
 
-# Create a security group for the RDS database
+# Security group for the Aurora-MySQL/RDS database
 resource "aws_security_group" "rds_sg" {
   name        = "coffee_rds_sg"
   description = "RDS database security group"
 
   vpc_id = aws_vpc.main.id
 
-  # Allow incoming traffic from the web servers
+#######  Caution #######  Caution #######  Caution ####### 
+# The databse is currently set to "wide open" # 
+# These must be changed to MySQL port number and subnets only # 
+#######  Caution #######  Caution #######  Caution ####### 
+  
+ # Incoming traffic from the web servers
   ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "x"
-    cidr_blocks = ["x.x.x.x/x"] # Subnet CIDR blocks
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"           # The databse is currently set to "wide open" #   
+    cidr_blocks = ["0.0.0.0/0"] # Subnet CIDR blocks
   }
 
-  # Allow outgoing traffic to the web servers
+  # Outgoing traffic to the web servers
   egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "x"
-    cidr_blocks = ["x.x.x.x/x"] # Subnet CIDR blocks
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # Subnet CIDR blocks
   }
 }
 
+#######  Caution #######  Caution #######  Caution ####### 
+# The databse is currently set to "wide open" # 
+# These must be changed to MySQL port number and subnets only # 
+#######  Caution #######  Caution #######  Caution ####### 
+  
