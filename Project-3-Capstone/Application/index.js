0b5b1d2357b0cd1b554fa3816c1f70f13d88700d
DJ322 <72959116+DJ322@users.noreply.github.com>
2023-07-06 11:59:15 -0500

    JS app script

--- Project-3-Capstone/Application/index.js
+++ Project-3-Capstone/Application/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+
 app.get('/', (req, res) => {
   const indexPath = path.join(__dirname, 'index.html');
   res.sendFile(indexPath);
